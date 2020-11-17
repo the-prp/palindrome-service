417cdd48208a362e45f642a52c25470629d73f53
prpic <Matthew.Prpic@nokia.com>
2020-11-17 13:13:11 -0500

    Adding basic tests for queue CRUD

--- test/PalindromeServiceTest.java
+++ test/PalindromeServiceTest.java
@@ -1,5 +1,3 @@
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -7,8 +5,105 @@ import com.palindrome.PalindromeService;
 
 public class PalindromeServiceTest extends PalindromeService {
     
+    
+    @Test
+    public void basicTest() {
+        
+        String validPalindrome = "racecar";
+        String invalidPalindrome = "Not a palindrome";
+        
+        System.out.println("Trying to add an invalid palindrome to the queue.");
+        
+        try {
+            createMessage(invalidPalindrome);
+        } catch (Exception e) {
+            Assert.assertFalse(messages.size() > 0);
+        }
+        
+        System.out.println("Trying to add a valid palindrome to the queue.");
+        
+        try {
+            createMessage(validPalindrome);
+        } catch (Exception e) {
+            Assert.assertTrue(messages.contains(validPalindrome));
+        }
+        
+        System.out.println("Trying to add an empty string to the queue.");
+        
+        try {
+            createMessage("");
+        } catch (Exception e) {
+            Assert.assertFalse(messages.contains(""));
+        }
+        
+        System.out.println("Trying to add null to the queue.");
+        
+        try {
+            createMessage(null);
+        } catch (Exception e) {
+            Assert.assertFalse(messages.contains(null));
+        }
+        
+        System.out.println("Printing out current queue:\n" + getMessages() + "\n\n");
+        
+        // replace the palindrome to test updates 
+        System.out.println(String.format("Trying to replace [%s] with [%s] in the queue.", validPalindrome, invalidPalindrome));
+        
+        try {
+            updateMessage(validPalindrome, invalidPalindrome);
+        } catch (Exception e) {
+            Assert.assertTrue(messages.contains(validPalindrome));
+            Assert.assertFalse(messages.contains(invalidPalindrome));
+            System.out.println("Message was not replaced due to it being an invalid palindrome.");
+        }
+
+        System.out.println("Printing out current queue:\n" + getMessages() + "\n\n");
+        
+        String validPalindromeReplacement = "kayak";
+        
+        try {
+            updateMessage(validPalindrome, validPalindromeReplacement);
+        } catch (Exception e) {
+            Assert.assertFalse(messages.contains(validPalindrome));
+            Assert.assertTrue(messages.contains(validPalindromeReplacement));
+            System.out.println("Message was not replaced due to it being an invalid palindrome.");
+        }
+        
+        System.out.println("Printing out current queue:\n" + getMessages() + "\n\n");
+        
+        // remove the palindrome to test deletes
+        System.out.println(String.format("Removing %s from the queue", validPalindromeReplacement));
+        
+        deleteMessage(validPalindromeReplacement);
+        
+        System.out.println("Printing out current queue:\n" + getMessages() + "\n\n");
+        
+        Assert.assertFalse(messages.contains(validPalindromeReplacement));
+        
+        // re add the palindrome for the final clear test
+        try {
+            createMessage(validPalindrome);
+        } catch (Exception e) {
+            Assert.fail();
+        }
+        
+        Assert.assertTrue(messages.contains(validPalindrome));
+
+        System.out.println("Printing out current queue:\n" + getMessages() + "\n\n");
+
+        
+        System.out.println("Clearing the message queue.");
+        
+        clearMessages();
+        
+        Assert.assertTrue(messages.size() == 0);
+        
+        System.out.println("Printing out current queue:\n" + getMessages() + "\n\n");
+    }
+    
     /**
-     * Tests message capacity
+     * Tests message capacity. Generates a larger than queue size
+     * amount of messages to add to prove the rotation of the queue works
      */
     @Test
     public void capacityTest() {
@@ -31,7 +126,7 @@ public class PalindromeServiceTest extends PalindromeService {
 
             System.out.println(getMessages());
 
-            Assert.assertEquals(messageQueueSize, messages);
+            Assert.assertEquals(messageQueueSize, messages.size());
         } catch (Exception e) {
             Assert.fail("Failure in capacity test during message creation.");
         }
@@ -57,7 +152,7 @@ public class PalindromeServiceTest extends PalindromeService {
             createMessage(longString);
         } catch (Exception e) {
             System.out.println("Message was not accepted as expected");
-            assertTrue(messages.size() == 0);
+            Assert.assertTrue(messages.size() == 0);
         }
     }
 }
